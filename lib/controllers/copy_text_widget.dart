6db44fefc39990f8a5a86e7da356350fcedaa9e6
Dhimas Dewanto <dhimasbrd@gmail.com>
2023-05-15 17:57:10 +0700

    Add shared pref in CopyTextWidget

--- lib/controllers/copy_text_widget.dart
+++ lib/controllers/copy_text_widget.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'copy_text_state.dart';
+
+const _keyIsCopy = "is_copy";
 
 class CopyTextWidget extends StatefulWidget {
   const CopyTextWidget({
@@ -15,18 +18,50 @@ class CopyTextWidget extends StatefulWidget {
 }
 
 class _CopyTextWidgetState extends State<CopyTextWidget> {
-  var _isCopy = false;
+  bool? _isCopy;
 
-  void _onChange(bool isCopy) {
+  @override
+  void initState() {
+    super.initState();
+
+    /// Run after build.
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await _initIsCopy();
+    });
+  }
+
+  Future<void> _initIsCopy() async {
+    final pref = await SharedPreferences.getInstance();
+    final isCopyString = pref.getString(_keyIsCopy);
+    final isCopy = isCopyString == "true" ? true : false;
     setState(() {
       _isCopy = isCopy;
     });
   }
 
+  Future<void> _saveIsCopy(bool isCopy) async {
+    final pref = await SharedPreferences.getInstance();
+    final isCopyString = isCopy ? "true" : "false";
+    await pref.setString(_keyIsCopy, isCopyString);
+  }
+
+  void _onChange(bool isCopy) {
+    setState(() {
+      _isCopy = isCopy;
+    });
+
+    _saveIsCopy(isCopy);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isCopy = _isCopy;
+    if (isCopy == null) {
+      return const SizedBox();
+    }
+
     return CopyTextState(
-      isCopy: _isCopy,
+      isCopy: isCopy,
       onChange: _onChange,
       child: widget.child,
     );
